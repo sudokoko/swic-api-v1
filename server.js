dbda06e79bf0fe42d0ddca1b3c279f931a583e06
koko <joebiden@kitgaming67.com>
2022-11-15 21:18:33 +0000

    src

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -30,11 +31,11 @@ app.get('/api/v1/webhook', function(req, res) {
             "content": '```'+ contents + '```'
         }
     };
-  
-    if (authentication === $API_V1_WEBHOOK_TOKEN) {
-        request(options);
-        return res.status(200).send({
-            response: 'Message sent.',
+
+    if (authentication === "thisEndpointDoesNotWork") {
+        // request(options);
+        return res.status(500).send({
+            response: 'This endpoint has been disabled..',
         });
     }
     else {
@@ -44,5 +45,37 @@ app.get('/api/v1/webhook', function(req, res) {
     }
 });
 
-app.listen();
-console.log('Server is running.');
+// productmanager registration check
+app.get('/api/v1/productmanager/activation', function(req, res) {
+    var request = require('request');
+    var key = req.query.key;
+
+// CONFIDENTIAL ZONE - AUTH KEYS
+
+// ind.srm.ph : ind.ph.interim_octopus_1950
+
+    var validkeys = JSON.parse('["ind.ph.interim_octopus_1950"]')
+
+// CONFIDENTIAL ZONE - AUTH KEYS
+
+    if (validkeys.includes(key)) {
+        return res.status(200).send({
+            message: 'ProductManager is activated.'
+        });
+    }
+    else {
+        return res.status(401).send({
+            message: 'Authentication key is invalid or has been revoked. Delete your key.txt file and reactivate ProductManager with a valid key.'
+        });
+    }
+});
+
+// handle non existent endpoints
+app.get('*', function(req, res) {
+    res.status(404).send({
+       message: 'Endpoint does not exist.'
+    })
+});
+
+app.listen(6080);
+console.log('Server is running on port 8080.');
